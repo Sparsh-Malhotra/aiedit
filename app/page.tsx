436d104e46a1ef24bd95a634979731640f5d0565
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2024-10-31 18:53:41 +0530

    Created Image and Layers store

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 "use client"
 
 import Editor from "@/components/Editor";
+import {ImageStore} from "@/store/image-store";
+import {LayerStore} from "@/store/layer-store";
 
 export default function Home() {
     return (
-        <main>
-            <Editor/>
-        </main>
+        <LayerStore.Provider initialValue={{
+            layerComparisonMode: false,
+            layers: []
+        }}>
+            <ImageStore.Provider initialValue={{generating: false}}>
+                <main>
+                    <Editor/>
+                </main>
+            </ImageStore.Provider>
+        </LayerStore.Provider>
     );
 }
