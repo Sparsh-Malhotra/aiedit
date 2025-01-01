fd66ac5e97f076795b2464fa708509a32f32cb82
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2025-01-01 21:07:55 +0530

    fix: move zustand store initialization to client-side

--- store/zustand-context.tsx
+++ store/zustand-context.tsx
@@ -4,20 +4,26 @@ import React from "react";
 export const createZustandContext = <TInitial, TStore extends StoreApi<any>>(
     getStore: (initial: TInitial) => TStore
 ) => {
-    const Context = React.createContext(null as any as TStore)
+    const Context = React.createContext<TStore | null>(null);
 
     const Provider = (props: {
-        children?: React.ReactNode
-        initialValue: TInitial
+        children?: React.ReactNode;
+        initialValue: TInitial;
     }) => {
-        const [store] = React.useState(getStore(props.initialValue))
+        const [store, setStore] = React.useState<TStore | null>(null);
 
-        return <Context.Provider value={store}>{props.children}</Context.Provider>
-    }
+        React.useEffect(() => {
+            setStore(getStore(props.initialValue));
+        }, [props.initialValue]);
+
+        if (!store) return null;
+
+        return <Context.Provider value={store}>{props.children}</Context.Provider>;
+    };
 
     return {
         useContext: () => React.useContext(Context),
         Context,
         Provider,
-    }
-}
+    };
+};
